ba942e59fe4c1f1ceeaf74037626593ef4878990
Esteban Trujillo <etrujillo@unimayor.edu.co>
2026-03-08 21:33:22 -0500

    se agrego la funcionaliad de redirigir

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';   
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
 export class Login {
-
+  constructor(private router: Router) { }
+  goToSignup() {
+    console.log("kwjnvkjen")
+    this.router.navigate(['/signup'])
+  }
 }
+
